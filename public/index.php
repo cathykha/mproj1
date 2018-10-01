e07177bec6f005501d4697e9f4f0a45db53352e7
cathykha <11232Hoc>
2018-10-01 17:56:08 -0400

    Feature: Using a factory (In Progress)

--- public/index.php
+++ public/index.php
@@ -5,4 +5,48 @@
  * Date: 10/1/18
  * Time: 3:31 PM
  */
-echo 'Hello World';
+
+main::start();
+class main {
+
+    static public function start() {
+        $records = csv::getRecords();
+        $table = html::generateTable($records);
+        system::printPage($table);
+    }
+
+}
+
+
+class csv {
+
+    static public function getRecords() {
+        $make = 'Ford';
+        $model = 'Tarus';
+        $car = AutomobileFactory::create($make, $model);
+
+        $records[] = $car;
+
+        print_r($records);
+
+        return $records;
+    }
+
+}
+
+class html {
+
+    static public function generateTable($records) {
+        $table = $records;
+        return $table;
+
+    }
+}
+
+class system {
+
+    static public function printPage($page) {
+        echo $page;
+
+    }
+}
